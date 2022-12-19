b5faa11eeb41a4060a0c77ddc0d7df9e2c4c374b
gubersky <https://github.com/gubersky/Burger.git>
2022-12-19 21:11:41 +0200

    update

--- src/test/java/api/test/ApiTest.java
+++ src/test/java/api/test/ApiTest.java
@@ -7,26 +7,38 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class TestSingleResource {
+public class ApiTest {
 
     private final static String URL = "https://reqres.in/";
 
     @Test
-    public void responseSingleResource() {
+    public void testSingleResource() {
 
         String text = "To keep ReqRes free, contributions towards server costs are appreciated!";
 
         ApiSpec.installSpec(ApiSpec.requestSpec(URL),
                 ApiSpec.responseSpec(200));
 
-       ResponseSingleResource responseSingleResource = given()
+        ResponseSingleResource responseSingleResource = given()
                 .when()
                 .get("api/unknown/2")
                 .then().log().all()
                 .extract().body().jsonPath()
-                       .getObject("support", ResponseSingleResource.class);
+                .getObject("support", ResponseSingleResource.class);
 
-       Assert.assertEquals(responseSingleResource.getText(),text);
+        Assert.assertEquals(responseSingleResource.getText(), text);
 
+    }
+
+    @Test
+    public void singleUserNotFound() {
+
+        ApiSpec.installSpec(ApiSpec.requestSpec(URL),
+                ApiSpec.responseSpec(404));
+
+        given()
+                .when()
+                .get("api/users/23")
+                .then().log().all();
     }
 }
